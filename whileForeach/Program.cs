8e2e4c91f8aa3e221499330e44977b26dba784b2
omerserfice <srfcomr@gmail.com>
2022-01-29 19:31:03 +0300

    while loop and foreeach

---
+++ whileForeach/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace whileForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Sayı giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+            int i=0;
+            int toplam = 0;
+            while(i<=sayi){
+                toplam += i;
+                i++;
+            }
+            System.Console.WriteLine("Toplam:"+toplam);
+
+            string[] arabalar = {"Opel","Mustang","Wolkswagen","Toyota"};
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
