e41fe9e2f80990f6febea2b9797d6c678450576f
omerserfice <srfcomr@gmail.com>
2022-01-29 17:32:45 +0300

    trycatch yapıldı.

---
+++ trycatch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace trycatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+           {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+
+                int c = a+b;
+                 System.Console.WriteLine(c);
+           }
+           catch (Exception ex)
+           {
+               System.Console.WriteLine("Bir hata oluştu:"+ex.Message);
+               
+           }
+           finally{
+               System.Console.WriteLine("İşlem tamamlandı.");
+           }
+
+        }
+    }
+}
