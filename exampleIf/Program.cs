4ca6331b6e720e317123e806f9c4758526e1eb2b
omerserfice <srfcomr@gmail.com>
2022-01-29 17:55:37 +0300

    iflere değinildi.

---
+++ exampleIf/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exampleIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time>6 && time<11)
+            {
+                System.Console.WriteLine("Günaydın");
+            }else if (time<=18)
+            {
+                System.Console.WriteLine("İyi Günler");
+            }
+            else{
+                System.Console.WriteLine("İyi geceler");
+            }
+
+            string sonuc = time<=18 ? "İyigünler" : "iyi geceler";
+            System.Console.WriteLine(sonuc);
+        }
+    }
+}
