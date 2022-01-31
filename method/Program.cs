df31c5fe484a5795c207f9ad4b3885af887f7c7d
omerserfice <srfcomr@gmail.com>
2022-01-31 14:33:52 +0300

    method

---
+++ method/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace method
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            int a = 2;
+            int b= 3;
+            int sonuc = Topla(a,b);
+            System.Console.WriteLine(sonuc);
+
+        Metotlar m=new Metotlar();
+        m.EkranaYazdır(sonuc.ToString());
+        
+        int sonuc2 = m.ArttirVeTopla(ref a,ref b);
+        m.EkranaYazdır(sonuc.ToString());
+        m.EkranaYazdır(sonuc2.ToString());
+        }
+
+        static int Topla(int deger1,int deger2){
+            return deger1+deger2;
+        }
+    }
+
+     class Metotlar{
+       public void EkranaYazdır(string veri){
+            System.Console.WriteLine(veri);
+        }
+
+        public int ArttirVeTopla(ref int deger1,ref int deger2){
+            deger1+=1;
+            deger1+=1;
+
+            return deger1+deger2;
+        }
+    }
+}
